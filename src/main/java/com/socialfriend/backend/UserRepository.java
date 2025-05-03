760f1818831fcf7a652d6727d1e582f5846992af
Kimberly Tom <kimberlyjtom@gmail.com>
2025-05-02 22:08:28 -0700

    Add query for getting followers

--- src/main/java/com/socialfriend/backend/UserRepository.java
+++ src/main/java/com/socialfriend/backend/UserRepository.java
@@ -1,8 +1,14 @@
 package com.socialfriend.backend;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends Neo4jRepository<User, Long> {
     Optional<User> findByUsername(String username);
+    @Query("MATCH (follower:User)-[:FOLLOWS]->(followee:User) WHERE followee.username = $username RETURN follower")
+    List<User> findFollowersByUsername(@Param("username") String username);
 }
